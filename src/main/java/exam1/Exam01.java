fecf23cc04cbed588f6289b569452dfa49253158
Padungsak Kloynimnuan <padungsak.k@kasikornbank.com>
2017-07-25 09:13:51 +0700

    code สายย่อ ๅ

--- src/main/java/exam1/Exam01.java
+++ src/main/java/exam1/Exam01.java
@@ -1,80 +1,66 @@
 package exam1;
 
 public class Exam01 {
-	int countA = 0;
-	int countB = 0;
-	boolean deuce = false;
-	String playerNameA;
-	String playerNameB;
-	String score;
-	String scoreA;
-	String scoreB;
+    boolean deuce = false;
+    private int countA = 0;
+    private int countB = 0;
+    private String playerNameA;
+    private String playerNameB;
+    private String score;
+    private String scoreA;
+    private String scoreB;
 
-	public Exam01(String firstPerson, String secondPerson) {
-		playerNameA = firstPerson;
-		playerNameB = secondPerson;
-	}
+    public Exam01(String firstPerson, String secondPerson) {
+        playerNameA = firstPerson;
+        playerNameB = secondPerson;
+    }
 
-	public void playerAWin() {
-		countA++;
-		if (countB == 0) {
-			scoreB = "Love";
-		}
-		if (countA == 1) {
-			scoreA = "Fifteen";
-		}
-		if (countA == 2) {
-			scoreA = "Thirty";
-		}
-		if (countA == 3) {
-			scoreA = "Forty";
-		}
-	}
+    public void playerAWin() {
+        countA++;
+    }
 
-	public void playerBWin() {
-		countB++;
-		if (countA == 0) {
-			scoreA = "Love";
-		}
-		if (countB == 1) {
-			scoreB = "Fifteen";
-		}
-		if (countB == 2) {
-			scoreB = "Thirty";
-		}
-		if (countB == 3) {
-			scoreB = "Forty";
-		}
-	}
+    public void playerBWin() {
+        countB++;
+    }
 
-	public String getScore() {
+    public String convertString(int count) {
+        if (count == 1) {
+            return "Fifteen";
+        } else if (count == 2) {
+            return "Thirty";
+        } else if (count == 3) {
+            return "Forty";
+        } else
+            return "Love";
+    }
 
-		if (countA > 3 || countB > 3) {
-			if (countB - countA == 1) {
-				score = "Advantage " + playerNameB;
-			} else if (countA - countB == 1) {
-				score = "Advantage " + playerNameA;
-			} else if (countA - countB >= 2) {
-				score = "Win for " + playerNameA;
-			} else if (countB - countA >= 2) {
-				score = "Win for " + playerNameB;
-			} else {
-				score = "Deuce";
-			}
-		} else if (countA == 0 && countB == 0) {
-			score = "Love-All";
-		} else if (countA == 1 && countB == 1) {
-			score = "Fifteen-All";
-		} else if (countA == 2 && countB == 2) {
-			score = "Thirty-All";
-		} else if (countA == countB) {
-			score = "Deuce";
-		} else {
-			score = scoreA + "-" + scoreB;
-		}
+    public String getScore() {
 
-		return score;
-
-	}
-
+        scoreA = convertString (countA);
+        scoreB = convertString (countB);
+        if (countA > 3 || countB > 3) {
+            if (countB - countA == 1) {
+                score = "Advantage " + playerNameB;
+            } else if (countA - countB == 1) {
+                score = "Advantage " + playerNameA;
+            } else if (countA - countB >= 2) {
+                score = "Win for " + playerNameA;
+            } else if (countB - countA >= 2) {
+                score = "Win for " + playerNameB;
+            } else {
+                score = "Deuce";
+            }
+        } else if (countA == 0 && countB == 0) {
+            score = "Love-All";
+        } else if (countA == 1 && countB == 1) {
+            score = "Fifteen-All";
+        } else if (countA == 2 && countB == 2) {
+            score = "Thirty-All";
+        } else if (countA == countB) {
+            score = "Deuce";
+        } else {
+            score = String.format ("%s-%s", scoreA, scoreB);
+        }
+        return score;
+    }
 }
